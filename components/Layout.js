e7da8a8cf207e228fa83333a4ddc1ff9abfd95bf
MakotoInomata <inomar1486@gmail.com>
2019-04-23 00:49:33 +0900

    add head

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Head from 'next/head';
 
 import 'normalize.css';
 import Header from './Header';
@@ -8,6 +9,9 @@ export default class Layout extends Component {
     const { children } = this.props;
     return (
       <div>
+        <Head>
+          <title>不定期更新症候群</title>
+        </Head>
         <Header />
         { children }
       </div>
