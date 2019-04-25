fb06b8f5317b692c21fd234a5c668bab2c7c2601
MakotoInomata <inomar1486@gmail.com>
2019-04-25 17:11:34 +0900

    bugfix

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const ENDPOINT = process.env.NODE_ENV !== 'production' ? 'https://post.inomar.me' : 'http://localhost:1337';
+export const ENDPOINT = process.env.NODE_ENV === 'production' ? 'https://post.inomar.me' : 'http://localhost:1337';
 export const GITHUB_URL = 'https://github.com/inomar';
 export const TWITTER_URL = 'https://twitter.com/macoto_chan';
