4c1de28ac8bdf8c699a2fbf862b9c5e76e21601b
MakotoInomata <inomar1486@gmail.com>
2019-04-25 19:02:31 +0900

    add meta & favicon

--- components/Layout.js
+++ components/Layout.js
@@ -11,8 +11,23 @@ export default class Layout extends Component {
     return (
       <div>
         <Head>
-          <title>不定期更新症候群</title>
+          <title>不定期更新症候群 ~ フルスタックエンジニアを目指して ~ </title>
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico"></link>
+          <meta name="description" content="不定期更新のwebエンジニアいのまーのブログです。フルスタックエンジニアを目指して日々奮闘しております。" />
+{/* 
+          <meta property="og:url" content="ページのURL" />
+          <meta property="og:title" content="ページのタイトル" />
+          <meta property="og:type" content="ページのタイプ">
+          <meta property="og:description" content="記事の抜粋" />
+          <meta property="og:image" content="画像のURL" />
+          <meta name="twitter:card" content="カード種類" />
+          <meta name="twitter:site" content="@Twitterユーザー名" />
+          <meta property="og:site_name" content="サイト名" />
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="fb:app_id" content="appIDを入力" /> */}
         </Head>
         <Header />
         { children }
