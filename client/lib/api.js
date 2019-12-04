5cb2746d9f655a8ff7583db96d4d545e8366c3c6
MakotoInomata <inomar1486@gmail.com>
2019-12-04 18:27:42 +0900

    feat prismic.io

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,10 +1,9 @@
-import WPAPI from 'wpapi';
 import axios from 'axios';
 
 class Api {
   constructor() {
-    this.baseEndpoint = 'https://blog-inomar-me.cdn.prismic.io/api/v2';
-    this.accessToken = 'MC5YZWRqU3hFQUFDc1lTclBF.Me-_vQQr77-9e--_vWQV77-977-9C3bvv73vv73vv73vv73vv73vv71A77-9GVbvv70r77-9Ru-_vUZE77-9Jw';
+    this.baseEndpoint = process.env.BASE_URL;
+    this.accessToken = process.env.ACCESS_TOKEN;
   }
 
   async getRef() {
@@ -17,7 +16,6 @@ class Api {
     }
   }
 
-
   async getPost(id) {
     await this.getRef();
     return await axios.get(`${this.apiEndpoint}&q=[[at(document.id,"${id}")]]`);
@@ -26,19 +24,6 @@ class Api {
   async getPosts(per = 10, page = 1) {
     await this.getRef();
     return await axios.get(`${this.apiEndpoint}&page=${page}`);
-  }
-
-  getCategories() {
-    return this.wp.categories();
-  }
-
-  getCategory(id) {
-    return this.wp.categories().id(id);
-  }
-
-  searchCases() {
-    this.endpoint.searchCases = this.endpoint.registerRoute('wp/v2','/cases/(?P<id>[0-9]+)');
-    return this.endpoint.searchCases();
   }
 }
 
