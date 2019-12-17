afeb8272d90b0dcd03f196c2e6e5cdd7c9acfed1
MakotoInomata <inomar1486@gmail.com>
2019-12-17 12:28:05 +0900

    fix purgeCSS

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 const withSass = require('@zeit/next-sass');
+const withPurgeCss = require('next-purgecss');
 
-module.exports = withSass({
-  webpack (config, options) {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-        },
-      }
-    })
-    return config;
-  },
-  target: 'serverless',
-});
+module.exports = withSass(
+  withPurgeCss(
+    {
+      webpack (config, options) {
+        config.module.rules.push({
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+            },
+          }
+        })
+        return config;
+      },
+      target: 'serverless',
+    }
+  )
+);
