f188b8baa198ea3ef8eda063ea06da76ab635fc7
MakotoInomata <inomar1486@gmail.com>
2020-03-09 23:57:19 +0900

    convert from About class component to functional component

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
-
 import Layout from '../components/layouts/Layout';
 import Prismic from 'prismic-javascript';
 import { apiEndpoint, accessToken } from '../lib/prismic-configuration';
 import HtmlSerializer from '../lib/htmlSerializer';
 import Box from '../components/atoms/Box';
 
-export default class About extends Component {
-  static async getInitialProps({ req }) {
-    const about = await this.getAboutPage(req);
-    return { doc: about }
-  }
-
-  static async getAboutPage(req) {
-    const API = await Prismic.getApi(apiEndpoint, { req, accessToken });
-    return await API.getSingle('profile');
-  }
-
-  render() {
-    const { doc } = this.props;
-    return (
-      <Layout title={'about'}>
-        <Box>
-          { HtmlSerializer(doc.data.about)}
-        </Box>
-      </Layout>
-    )
-  }
+const About = ({ doc }) => {
+  return (
+    <Layout title={'about'}>
+      <Box>
+        { HtmlSerializer(doc.data.about)}
+      </Box>
+    </Layout>
+  )
 }
+
+
+About.getInitialProps = (async ({ req }) => {
+  const API = await Prismic.getApi(apiEndpoint, { req, accessToken });
+  const doc = await API.getSingle('profile');
+  return { doc };
+});
+
+export default About;
