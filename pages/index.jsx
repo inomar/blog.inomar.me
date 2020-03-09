e6e1037a199d308b97525e725e014858de202f3b
MakotoInomata <inomar1486@gmail.com>
2020-03-09 23:52:32 +0900

    convert from Index class component to functional component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'react-grid-system';
+import { Row } from 'react-grid-system';
 import Prismic from 'prismic-javascript';
 
 import Article from '../components/molecules/Article';
 import Pagenation from '../components/molecules/Pagination';
 import { postFormatter } from '../lib/formatter';
 import Client from '../lib/prismicHelpers';
-import Layout from '../components/layouts/Layout'
+import Layout from '../components/layouts/Layout';
 
-export default class Index extends Component {
-  static async getInitialProps({ req, query }) {
-    const { page } = query;
-    let posts = await Client(req).query(
-      Prismic.Predicates.at('document.type', 'blogpost'),
-      { pageSize : 10, page }
-    )
-    const { next_page, prev_page } = posts; 
-    const currentPage = posts.page;
-    posts = posts.results.map(post => postFormatter(post))
-    return {　posts, next_page, prev_page, page: currentPage };
-  }
-
-  render() {
-    const { posts, next_page, prev_page, page } = this.props;
+const Index = props => {
+  const { posts, next_page, prev_page, page } = props;
     return (
       <Layout title="">
         
@@ -36,5 +22,18 @@ export default class Index extends Component {
         </Row>
       </Layout>
     )
-  }
 }
+
+Index.getInitialProps = ( async ({ req, query }) => {
+  const { page } = query;
+  let posts = await Client(req).query(
+    Prismic.Predicates.at('document.type', 'blogpost'),
+    { pageSize : 10, page }
+  )
+  const { next_page, prev_page } = posts; 
+  const currentPage = posts.page;
+  posts = posts.results.map(post => postFormatter(post))
+  return {　posts, next_page, prev_page, page: currentPage };
+});
+
+export default Index;
